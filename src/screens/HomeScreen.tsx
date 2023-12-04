8f625a9d48b13a5c3c500bd32c9f0dc5c8749cc9
taras-doloshytskyi <taras.doloshytskyi.work@gmail.com>
2023-12-04 10:54:34 +0200

    added modal

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from '../components/Header';
-import TabButtons from '../components/TabButtons';
-import Quests from '../components/Quests';
-import BottomTabs from '../components/BottomTabs';
-import Tags from '../components/Tags';
+import React, { useState, useEffect } from 'react';
+import { Modal, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { useDrawerStatus } from '@react-navigation/drawer';
 import { BlurView } from 'expo-blur';
-
+import Header from '../components/Header';
+import TabButtons from '../components/TabButtons';
+import Tags from '../components/Tags';
+import Quests from '../components/Quests';
+import BottomTabs from '../components/BottomTabs';
 
 export default function Home() {
-
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const isDrawerOpen = useDrawerStatus() === 'open';
+  const [modalVisible, setModalVisible] = useState(isDrawerOpen);
 
   const handleTabPress = (index) => {
     setActiveTabIndex(index);
   };
-  
+
+  useEffect(() => {
+    if (isDrawerOpen) {
+      setModalVisible(!modalVisible);
+    }
+  }, []);
+
   return (
-    <BlurView intensity={isDrawerOpen ? 70 : 0} style={styles.container}>
-      <Header />
-      <TabButtons/>
-      <Tags/>
-      <Quests/>
-      <BottomTabs onTabPress={handleTabPress} />
-    </BlurView>
+    <>
+      <Modal visible={modalVisible} animationType='fade' transparent={true} style={styles.modalOverlay}/>
+      <View style={styles.container}>
+        <Header />
+        <TabButtons />
+        <Tags />
+        <Quests />
+        <BottomTabs onTabPress={handleTabPress} />
+      </View>
+      {isDrawerOpen ? (
+        <BlurView intensity={5} style={[StyleSheet.absoluteFill, { zIndex: 1 }]} />
+      ) : null}
+    </>
   );
 }
 
@@ -35,5 +46,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#2A3141',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalOverlay: {
+    flex: 1,
   },
 });
